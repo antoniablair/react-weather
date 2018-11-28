82c15314aec322f683e6cec90fb7c8b811a728c8
Antonia Blair <antoniablair@gmail.com>
2018-11-28 11:16:24 -0500

    Temporarily remove box shadows

--- src/components/Card.js
+++ src/components/Card.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
   bottomElement: {
     borderBottomLeftRadius: '5px',
     borderBottomRightRadius: '5px',
-    boxShadow: '0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2)',
+    // boxShadow: '0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2)',
   },
   imageContainer: {
     height: '175px',
